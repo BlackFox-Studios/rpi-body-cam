eb786c73e99edc6585be2b832624d29c85e60dee
nosrac77 <carson.newton@outlook.com>
2018-10-22 18:20:31 -0700

    updated body_cam.py with comments so that comrades can know what's going on in this module.

--- src/body_cam.py
+++ src/body_cam.py
@@ -1,16 +1,24 @@
-"""Module which holds PiCamera configurations."""
+"""Module which holds PiCamera and PIR Sensor configurations."""
 
-"""."""
+# Lines 5-7 import necessary Python libraries
 
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep, asctime
 
+# Lines 11-12 define variables for motion-detecting PIR sensor.
+
 pir_sensor = 11
 current_state = 0
 
+# Lines 16-17 configure the RPi's breadboard
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pir_sensor, GPIO.IN)
+
+# Lines below are responsible for running the motion-detecting PIR sensor and
+# triggering the camera to record video if motion is detected. The script below
+# will continuously execute until 'Ctrl+C' is pressed on the user's keyboard.
 
 try:
     while True:
@@ -27,4 +35,3 @@ try:
 except(KeyboardInterrupt):
     print('Monitoring process: terminated.')
     GPIO.cleanup()
-
