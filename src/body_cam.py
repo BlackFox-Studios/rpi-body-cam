29876c7ba201afe214d441b09ce7cd198b87a0ad
nosrac77 <carson.newton@outlook.com>
2018-11-01 18:19:15 -0700

    body_cam.py now has implementations of a button and an LED light. Filming begins when the button is pressed and ends when the button is pressed again.

--- src/body_cam.py
+++ src/body_cam.py
@@ -6,34 +6,36 @@ import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep, asctime
 
-# Lines 11-12 define variables for motion-detecting PIR sensor.
+button = 6
+led = 18
 
-pir_sensor = 11
-current_state = 0
-
-# Lines 16-17 configure the RPi's breadboard
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pir_sensor, GPIO.IN)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(button, GPIO.IN)
 
 # Lines below are responsible for running the motion-detecting PIR sensor and
 # triggering the camera to record video if motion is detected. The script below
 # will continuously execute until 'Ctrl+C' is pressed on the user's keyboard.
 
-# TODO: Add push-button functionality and an LED light
-
 try:
     while True:
-        sleep(0.1)
-        current_state = GPIO.input(pir_sensor)
-        if current_state == 1:
+        if GPIO.input(button):
+            sleep(1)
+            print('Button has been pressed.')
+            is_recording = 1
+            GPIO.output(led, GPIO.HIGH)
             with PiCamera() as camera:
-                camera.start_preview()
-                camera.annotate_text = 'Recording triggered by motion: {}'.format(asctime())
-                camera.start_recording('/home/pi/Desktop/motion_detector_test.h264')
-                sleep(10)
+                camera.annotate_text = 'Recording triggered by button: {}'.format(asctime())
+                print('Recording about to begin.')
+                camera.start_recording('/home/pi/Desktop/testVid.h264')
+                while is_recording:
+                    if GPIO.input(button):
+                        sleep(1)
+                        is_recording = 0
+                print('Button has been pressed again, recording is about to stop.')
                 camera.stop_recording()
-                camera.stop_preview()
+                GPIO.output(led, GPIO.LOW)
 except(KeyboardInterrupt):
     print('Monitoring process: terminated.')
     GPIO.cleanup()
